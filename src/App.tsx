008184e15f29e043219bc39ce790b8daf78b43cb
faelpatrick <faelpatrick@gmail.com>
2023-11-27 12:27:53 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import dotenv from "dotenv";
+
+import bookImage from "./assets/img/Capa_Livro_Quarto_de_Costura.png";
+import authorImage from "./assets/img/Autor_Livro_Quarto_de_Costura.png";
+import BtnComprar from "./components/BtnComprar";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="book-section">
+          <img src={bookImage} className="book-image" alt="Capa do livro Quarto de Costura" />
+          <div className="book-description">
+            <h2>Quarto de Costura</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <BtnComprar />
+          </div>
+        </div>
+        <div className="author-section">
+          <div className="author-description">
+            <h2>António Machado</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <img src={authorImage} className="author-image" alt="Foto do autor António Machado" />
+        </div>
+        <div className="sell-section">
+          <div className="sell-description">
+            <h2>Como adiquir seu livro</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="sell-btn">
+            <BtnComprar />
+          </div>
+          <br />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/BtnComprar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BtnComprar = () => {
+    return (
+        <div className="purchase-section">
+          <button className="purchase-button">Comprar Livro</button>
+        </div>
+    );
+};
+
+export default BtnComprar;
